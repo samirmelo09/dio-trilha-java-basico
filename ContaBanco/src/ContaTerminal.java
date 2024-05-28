8e034bd206d410944d6106c67d53d0531d41ba16
Samir Melo <samirp.melo@gmail.com>
2024-05-28 12:02:44 -0300

    segundo commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+        /* int conta = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "SAMIR MELO";
+        double saldo = 237.48;
+        */
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner input = new Scanner(System.in);
+        int conta;
+        String agencia;
+        String nomeCliente;
+        double saldo = 237.48;
+
+        System.out.println("Bem vindo ao Banco DIO, Por favor, informe o seu nome: ");
+        nomeCliente = input.next();
+        System.out.println("Por favor, digite o número da sua conta: ");
+        conta = input.nextInt();
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = input.next();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +  agencia +", conta " + conta + "e seu saldo de " + saldo + "já está disponível para saque." );
+        
+
+        input.close();
     }
 }
